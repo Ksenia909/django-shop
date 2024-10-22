23e93c778611410873ed919a2651d2b6b1c99794
Ksenia Berdugina <kseniaberdugina918@gmail.com>
2024-10-22 17:04:13 +0300

    Add product detail page and link products from the list

--- myshop/products/views.py
+++ myshop/products/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Product
 
@@ -7,3 +7,8 @@ from .models import Product
 def product_list(request):
     products = Product.objects.all()
     return render(request, 'products/product_list.html', {'products': products})
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'products/product_detail.html', {'product': product})
